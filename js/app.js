05c9d0c7d300bf61b66175dd8a16249b0698a540
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-04-02 15:33:12 -0600

    more progress

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 console.log('running')
 
+
+
+
+
+//========================
+// Clock
+//========================
+
+
 let timePassing;
 let seconds = 0;
 let minutes = 0;
@@ -12,15 +21,19 @@ const secondsGoUp = () => {
         console.log(timePassing);
     }
 }
-$('.start').click(function () {
+$('.start').on('click', () => {
     timePassing = setInterval(secondsGoUp, 1000);
     console.log('start was pressed');
-})
+});
 $('.stop').click(function () {
     clearInterval(timePassing);
-    console.log('stop was pressed');
+        console.log('stop was pressed');
 
 });
+
+
+
+// Class creation of the tomogachi. 
 
 
 class tomogachi{
@@ -30,21 +43,24 @@ class tomogachi{
         this.typeOfTomo = ''
     }
 }
+// ================================
+// starting and max values
+//=================================
 
-
-let boredom = 0;
+let boredom = 1;
 const maxBoredom = 10;
 
 // Sleepiness values
-let sleepiness = 0;
+let sleepiness = 1;
 const maxSleepiness = 10;
 
 
 // Hunger Values
-let hunger = 0;
+let hunger = 1;
 const maxHunger = 10;
 
 // timed events
+
 
 const hungryboi = () => {
 if(seconds % 3 === 0 ){
@@ -52,31 +68,51 @@ if(seconds % 3 === 0 ){
     console.log('hunger has gone up');
 
     if (hunger === maxHunger) {
-        alert('your Tomogachi has died! ;__;')
+        alert('your Tomogachi has died! ;__; あなたのトモガチは死んでました。哀しい！')
         clearInterval(timePassing);
     }
 }
+// if play is cilcked it resets the value of what it was at to 1. 
+$('feed').on('click', () => {
+    hunger = 1;
+});
 }
 const boredboi = () => {
 if(seconds % 2 === 0){
     console.log('your tomogachi is getting bored');
     boredom++;
     if (boredom === maxBoredom) {
-        alert('your Tomogachi has died! ;__;')
+        alert('your Tomogachi has died! ;__; あなたのトモガチは死んでました。哀しい！')
         clearInterval(timePassing);
-
     }
 }
+// if play is cilcked it resets the value of what it was at to 1. 
+$('.play').on('click', () => {
+     boredom = 1;
+});
 }
 const sleepyboi = () => {
 if(seconds % 4 === 0){
     console.log('your tomogachi is getting sleepy');
     sleepiness++;
     if (sleepiness === maxSleepiness) {
-        alert('your Tomogachi has died! ;__;')
+        alert('your Tomogachi has died! ;__; あなたのトモガチは死んでました。哀しい！')
         clearInterval(timePassing);
     }
+    // if play is cilcked it resets the value of what it was at to 1. 
+$('.sleep').on('click', () => {
+     sleepiness = 1;
+});
 }
+}
+const aging = () => {
+    if(seconds % 15 === 0){
+        age++;
+    }
+    if(age === 30) {
+        alert('your Tomogachi has died of old age. ;__; あなたのトモガチは死んでました。哀しい！')
+        clearInterval(timePassing);
+    }
 }
 
 

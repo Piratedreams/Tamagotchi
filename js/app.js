01116791063f6c3b2aef962dbf6d8c07f23c2577
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-04-04 13:27:47 -0600

    commit b4 breaking

--- js/app.js
+++ js/app.js
@@ -33,9 +33,13 @@ $('.stop').click(function () {
         console.log('stop was pressed');
 
 });
+// const statUpdate = () => {
 
-
-
+//  attributes();
+//  hungryboi();
+//  boredboi();
+//  sleepyboi();
+// }
 // Class creation of the tomogachi. 
 
 
@@ -47,115 +51,77 @@ class tomogachi{
 }
 const tomo = new tomogachi('shyboi ', 0);
 
- name = $('#input-name').val();
+//  name = $('#input-name').val();
 
 // ================================
 // starting and max values
 //=================================
 
 let boredom = 1;
-const maxBoredom = 10;
-
-// Sleepiness values
 let sleepiness = 1;
-const maxSleepiness = 10;
-
-
-// Hunger Values
 let hunger = 1;
-const maxHunger = 10;
-
 let age = 0;
-const maxAge = 30;
-// timed events
-
 
 const hungryboi = () => {
-if(seconds % 3 === 0 ){
-    hunger++;
-    console.log('hunger has gone up');
-    $('.hunger').text(hunger);
-}
-    if (hunger === maxHunger) {
+ if (hunger === 10) {
         alert('your Tomogachi has died! ;__; あなたのトモガチは死んでました。哀しい！')
         clearInterval(timePassing);
     }
-        
 }
-$('.hunger').text(hunger);
-// if feed is cilcked it resets the value of what it was at to 1. 
+hungryboi();
+const boredboi = () => {
+ if (boredom === 10) {
+        alert('your Tomogachi has died! ;__; あなたのトモガチは死んでました。哀しい！')
+        clearInterval(timePassing);
+    }
+}
+boredboi();
+const sleepyboi = () => {
+ if (sleepiness === 10) {
+        alert('your Tomogachi has died! ;__; あなたのトモガチは死んでました。哀しい！')
+        clearInterval(timePassing);
+    }
+};
+sleepyboi();
+const aging = () => {
+     if(age === 30) {
+        alert('your Tomogachi has died of old age. ;__; あなたのトモガチは死んでました。哀しい！')
+        clearInterval(timePassing);
+    }
+};
+aging();
+// Button pressing Functions
+// if play is cilcked it resets the value of what it was at to 1. 
+$('.play').on('click', () => {
+    boredom = 1;
+});
+$('.sleep').on('click', () => {
+    sleepiness = 1;
+});
 $('feed').on('click', () => {
     hunger = 1;
 });
 
-const boredboi = () => {
-if(seconds % 2 === 0){
-    console.log('your tomogachi is getting bored');
-    boredom++;
-    $('.boredom').text(boredom);
+// Display all of the stats
+const attributes = () => {
+     if (seconds % 10 === 0) {
+         age++;
+         console.log('one year older.');
+         $('.age').text(age);
+     }
+     if (seconds % 3 === 0) {
+         console.log('your tomogachi is getting sleepy');
+         sleepiness++;
+         $('.sleepiness').text(sleepiness);
+     }
+     if (seconds % 2 === 0) {
+         console.log('your tomogachi is getting bored');
+         boredom++;
+         $('.boredom').text(boredom);
+     }
+     if (seconds % 3 === 0) {
+         hunger++;
+         console.log('hunger has gone up');
+         $('.hunger').text(hunger);
+     }
 }
-    if (boredom === maxBoredom) {
-        alert('your Tomogachi has died! ;__; あなたのトモガチは死んでました。哀しい！')
-        clearInterval(timePassing);
-    }
-}
-// if play is cilcked it resets the value of what it was at to 1. 
-$('.play').on('click', () => {
-     boredom = 1;
-});
-
-boredboi();
-const sleepyboi = () => {
-if(seconds % 4 === 0){
-    console.log('your tomogachi is getting sleepy');
-    $('.sleepiness').text(sleepiness);
-    sleepiness++;
-};
-    if (sleepiness === maxSleepiness) {
-        alert('your Tomogachi has died! ;__; あなたのトモガチは死んでました。哀しい！')
-        clearInterval(timePassing);
-    };
-     // make the lights turn off for the tomogachi.
-};
-sleepyboi();
-
-$('.sleep').on('click', () => {
-            sleepiness = 1;
-});
-
-const aging = () => {
-    if(seconds % 15 === 0){
-        age++;
-        console.log('one year older.');
-        $('.age').text(age);
-    };
-    if(age === maxAge) {
-        alert('your Tomogachi has died of old age. ;__; あなたのトモガチは死んでました。哀しい！')
-        clearInterval(timePassing);
-    };
-};
-aging();
-
-// Time interval
-
-// let timePassing;
-// let seconds = 0;
-// let minutes = 0;
-// const secondsGoUp = () => {
-//     seconds++;
-//     $('.time').text(seconds)
-//     if (seconds % 60 === 0) {
-//         minutes++;
-//         $('.minutes').text(minutes);
-//         console.log(timePassing);
-//     }
-// }
-// $('.start').click(function () {
-//     timePassing = setInterval(secondsGoUp, 1000);
-//     console.log('start was pressed');
-// })
-// $('.stop').click(function () {
-//     clearInterval(timePassing);
-//         console.log('stop was pressed');
-
-// });
